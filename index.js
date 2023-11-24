c0868c1bab85901f76c425f99366d21b9808f7d0
JuliaLap <jul.lapshina2013@gmail.com>
2023-11-24 16:04:28 +0300

    GOTOVO

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const inquirer = require("inquirer");
+const { EOL } = require("os");
+const { questions, gameQuestions, gameQuestions2, themes } = require("./model");
+const checkAnswer = require("./check");
 
-class Hello {
-  constructor(type, name, message) {
-    this.type = type;
-    this.name = name;
-    this.message = message;
+async function startGame() {
+  const welcome = await inquirer.prompt(questions);
+  console.log(`Привет ${welcome.playerName}! Давай начнем игру!`);
+  const themesChoise = await inquirer.prompt(themes);
+
+  if (themesChoise.Type === "Природа") {
+    for (let index = 0; index < gameQuestions2.length; index += 1) {
+      let userAnswer = await inquirer.prompt(gameQuestions2[index]);
+
+      if (
+        userAnswer[gameQuestions2[index].name] === gameQuestions2[index].answer
+      ) {
+        console.log("Верно");
+      } else {
+        console.log("Неверно");
+      }
+    }
+  } else {
+    for (let index = 0; index < gameQuestions.length; index += 1) {
+      let userAnswer = await inquirer.prompt(gameQuestions[index]);
+
+      if (
+        userAnswer[gameQuestions[index].name] === gameQuestions[index].answer
+      ) {
+        console.log("Верно");
+      } else {
+        console.log("Неверно");
+      }
+    }
   }
 }
 
-// class Quiz {
-//   constructor(themes, questions, results) {
-//     this.themes = themes;
-//     this.questions = questions;
-//     this.results = results;
-//   }
-// }
-
-async function startGame() {
-  const r = await inquirer.prompt(Hello);
-  console.log;
-}
-
 startGame();
+
+module.exports = startGame;
