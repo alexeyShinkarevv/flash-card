200840d31b7e5a01df7a5f1f925474cef0da97c6
JuliaLap <jul.lapshina2013@gmail.com>
2023-11-24 11:31:36 +0300

    npm

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-git
+const inquirer = require("inquirer");
+
+class Hello {
+  constructor(type, name, message) {
+    this.type = type;
+    this.name = name;
+    this.message = message;
+  }
+}
+
+// class Quiz {
+//   constructor(themes, questions, results) {
+//     this.themes = themes;
+//     this.questions = questions;
+//     this.results = results;
+//   }
+// }
+
+async function startGame() {
+  const r = new Hello();
+  const welcome = await inquirer.prompt(Hello);
+  console.log("welcome");
+}
+
+startGame();
